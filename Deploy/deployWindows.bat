6090e304ac1af90c30c541df2aa2b8ebe7328e3f
Sriep <piers.shepperson@btinternet.com>
2017-05-17 17:20:09 +0100

    Fixed EventOrganiser Windows auto deply

--- Deploy/deployWindows.bat
+++ Deploy/deployWindows.bat
@@ -17,12 +17,12 @@ del /s /q %EVENTORGANISER_DIR%\*.h
 cd %OPENSSL64_DIR%
 c:
 copy *.dll %EVENTORGANISER_DIR%
-copy %QT_DIR%\bin\Qt5Location.dll %EVENTORGANISER_DIR%\Qt5Location.dll
-xcopy %QT_DIR%\qml\QtWebView %EVENTORGANISER_DIR%\QtWebView\*
-xcopy %QT_DIR%\qml\QtWebEngine %EVENTORGANISER_DIR%\QtWebEngine\*
-xcopy %QT_DIR%\qml\QtPositioning %EVENTORGANISER_DIR%\QtPositioning\*
-xcopy %QT_DIR%\qml\QtLocation %EVENTORGANISER_DIR%\QtLocation\*
-xcopy %QT_DIR%\plugins\geoservices %EVENTORGANISER_DIR%\geoservices\*
+copy /y  %QT_DIR%\bin\Qt5Location.dll %EVENTORGANISER_DIR%\Qt5Location.dll
+xcopy /y  %QT_DIR%\qml\QtWebView %EVENTORGANISER_DIR%\QtWebView\*
+xcopy /y  %QT_DIR%\qml\QtWebEngine %EVENTORGANISER_DIR%\QtWebEngine\*
+xcopy /y  %QT_DIR%\qml\QtPositioning %EVENTORGANISER_DIR%\QtPositioning\*
+xcopy /y  %QT_DIR%\qml\QtLocation %EVENTORGANISER_DIR%\QtLocation\*
+xcopy /y  %QT_DIR%\plugins\geoservices %EVENTORGANISER_DIR%\geoservices\*
 
 set INNO_DIR="C:\Program Files (x86)\Inno Setup 5"
 set DEPLOYDIR=C:\Deployment
@@ -32,7 +32,7 @@ set EO_ISS_FILE=EventOrganiser.iss
 REM rmdir /s /q %QUTDIR%
 
 cd %INNO_DIR%
-iscc /Q %MOBILE_DIR%/Deploy/%EO_ISS_FILE%
+iscc /Q %MOBILE_DIR%\Deploy\%EO_ISS_FILE%
 
 REM Event App deployment
 set WINDEPLOYQT_EXE=C:\Qt\5.8\msvc2015_64\bin\windeployqt.exe
@@ -40,19 +40,20 @@ set EVENTAPP_DIR=E:\Mobile\Mobile\build-EventApp-Desktop_Qt_5_8_0_MSVC2015_64bit
 set QML_DIR=E:\Mobile\Mobile\EventApp
 set OPENSSL64_DIR=C:\OpenSSL-Win64
 
-%WINDEPLOYQT_EXE% --qmldir %QML_DIR% %EVENTAPP_DIR%\EventAppexe
+%WINDEPLOYQT_EXE% --qmldir %QML_DIR% %EVENTAPP_DIR%\EventApp.exe
 del /s /q %EVENTAPP_DIR%\*.obj
 del /s /q %EVENTAPP_DIR%\*.cpp
+del /s /q %EVENTAPP_DIR%\*.h
 
 cd %OPENSSL64_DIR%
 c:
 copy *.dll %EVENTAPP_DIR%
-copy %QT_DIR%\bin\Qt5Location.dll %EVENTAPP_DIR%\Qt5Location.dll
-xcopy %QT_DIR%\qml\QtWebView %EVENTAPP_DIR%\QtWebView\*
-xcopy %QT_DIR%\qml\QtWebEngine %EVENTAPP_DIR%\QtWebEngine\*
-xcopy %QT_DIR%\qml\QtPositioning %EVENTAPP_DIR%\QtPositioning\*
-xcopy %QT_DIR%\qml\QtLocation %EVENTAPP_DIR%\QtLocation\*
-xcopy %QT_DIR%\plugins\geoservices %EVENTAPP_DIR%\geoservices\*
+copy /y  %QT_DIR%\bin\Qt5Location.dll %EVENTAPP_DIR%\Qt5Location.dll
+xcopy /y  %QT_DIR%\qml\QtWebView %EVENTAPP_DIR%\QtWebView\*
+xcopy /y %QT_DIR%\qml\QtWebEngine %EVENTAPP_DIR%\QtWebEngine\*
+xcopy /y  %QT_DIR%\qml\QtPositioning %EVENTAPP_DIR%\QtPositioning\*
+xcopy /y  %QT_DIR%\qml\QtLocation %EVENTAPP_DIR%\QtLocation\*
+xcopy /y  %QT_DIR%\plugins\geoservices %EVENTAPP_DIR%\geoservices\*
 
 set INNO_DIR="C:\Program Files (x86)\Inno Setup 5"
 set DEPLOYDIR=C:\Deployment
@@ -62,4 +63,4 @@ set EA_ISS_FILE=EventApp.iss
 REM rmdir /s /q %QUTDIR%
 
 cd %INNO_DIR%
-iscc /Q %MOBILE_DIR%/Deploy/%EA_ISS_FILE%
+iscc /Q %MOBILE_DIR%\Deploy\%EA_ISS_FILE%
